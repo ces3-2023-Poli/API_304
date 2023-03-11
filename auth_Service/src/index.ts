c8a467cf10510f46d80a0bf796006c47f31060dd
Andres Henao <user.email andres_henao@elpoli.edu.co>
2023-03-11 08:09:33 -0500

    package modified

--- auth_Service/src/index.ts
+++ auth_Service/src/index.ts
@@ -1,1 +1,9 @@
-console.log("HOLA")
+import express, {Request, Response} from "express"
+const server = express()
+
+server.get('/api',(req: Request, res: Response)=>{
+    res.send({message: "hello wordl"})
+})
+server.listen(80,()=>{
+    console.log("Aplicación corriendo")
+})
